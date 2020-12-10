6c2c830bc91dd9c8911e077b931549ca507fa083
Alex Cebulski <Alex.Cebulski@viu.ca>
2020-12-10 14:25:20 -0800

    maya order the other way and calc watyr

--- mayamysql-importer.php
+++ mayamysql-importer.php
@@ -65,32 +65,9 @@ foreach ($csv as $line) {
 
 // Then update clean table //
 // temp change
-function getMySQLRows2($conn, $stationName, $numRows) {
-
-    $sql = "(SELECT * FROM `$stationName` ORDER BY DateTime asc LIMIT $numRows) order by DateTime";
-
-    $result = mysqli_query($conn,$sql);
-
-    if(!$result){
-        exit("Select Query Error Description: ". mysqli_error($conn));
-    }
-
-    // put query in assoc array 
-    $raw_array = array();
-
-    while($row = mysqli_fetch_assoc($result)) {
-        $raw_array[] = $row;
-        }
-
-    return $raw_array;
-}
 
 // get rows from mysql
-$rawRows = getMySQLRows2($conn, "raw_$tbl", $numToClean);
-
-
-
-
+$rawRows = getMySQLRows($conn, "raw_$tbl", $numToClean);
 
 $lineNum = 0;
 foreach ($rawRows as $line) {
