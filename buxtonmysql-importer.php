8326d16f2b5f9fafc6c1d2376ef67a122f6eba1c
Alex Cebulski <alexcebulski@gmail.com>
2024-01-13 12:50:12 -0700

    fix wrong sr50 offset

--- buxtonmysql-importer.php
+++ buxtonmysql-importer.php
@@ -102,7 +102,7 @@ foreach ($rawRows as $line) {
         "PP_Tipper" => $line["Rain"],
         "PC_Raw_Pipe" => $line["Pcp_GaugeLvl"] * 1000,
         "PP_Pipe" => $PP_Pipe,
-        "Snow_Depth" => ($line["Snow_Depth"] == 0 ? NAN : ($line["Snow_Depth"] - 3.66) * 100), // Check and adjust snow depth
+        "Snow_Depth" => ($line["Snow_Depth"] == 0 ? NAN : (3.66 - $line["Snow_Depth"]) * 100), // Check and adjust snow depth
         "Solar_Rad" => $line["SolarRad_Avg"],
         "Batt" => $line["BattVolt"]
     );
